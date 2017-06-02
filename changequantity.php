f7c3b3c7f4e0450759ee6bff3a2f4436842727d1
mrnarien <fable.3@yandex.ru>
2017-06-02 22:05:48 +0700

    Update changequantity.php

--- changequantity.php
+++ changequantity.php
@@ -3,16 +3,18 @@ session_start();
 if (isset($_SESSION['login']) && isset($_SESSION['adminmode'])) {
     echo header('Location:index.php');
 } else {
-    $changequantity='';
     for ($i = 0; $i < $_SESSION['basketcounter']; $i++) {
-        if ($_GET['changequantity' . $i] == '-' && $_SESSION['quantity' . $i] > 1) {
-            $_SESSION['quantity' . $i]--;
-            break;
+        if(!empty($_GET['changequantity' . $i])){
+                if ($_GET['changequantity' . $i] == '-' && $_SESSION['quantity' . $i] > 1) {
+                $_SESSION['quantity' . $i]--;
+                break;
+            }
+            if ($_GET['changequantity' . $i] == '+') {
+                $_SESSION['quantity' . $i]++;
+                break;
+            }
         }
-        if ($_GET['changequantity' . $i] == '+') {
-            $_SESSION['quantity' . $i]++;
-            break;
-        }
+       
     }
     echo '<script>location.href="', $_SERVER['HTTP_REFERER'], '"</script>';
 }
