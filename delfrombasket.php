126adcf797d510ce5affc610155f0a05830ce6bb
mrnarien <fable.3@yandex.ru>
2017-06-02 22:11:53 +0700

    Update delfrombasket.php

--- delfrombasket.php
+++ delfrombasket.php
@@ -16,12 +16,22 @@ if (isset($_SESSION['login']) && isset($_SESSION['adminmode'])) {
         }
     }
     for ($i = $itemnumber; $i < $_SESSION['basketcounter']; $i++) {
-        $next                       = $i + 1;
+    $next                       = $i + 1;
+    if(!empty($_SESSION['item' . $i])  && !empty($_SESSION['item' . $next])){
         $_SESSION['item' . $i]      = $_SESSION['item' . $next];
+    }
+    if(!empty($_SESSION['item_name' . $i])  && !empty($_SESSION['item_name' . $next])){
         $_SESSION['item_name' . $i] = $_SESSION['item_name' . $next];
+    }
+    if(!empty($_SESSION['price' . $i])  && !empty($_SESSION['price' . $next])){
         $_SESSION['price' . $i]     = $_SESSION['price' . $next];
+    }
+    if(!empty($_SESSION['quantity' . $i])  && !empty($_SESSION['quantity' . $next])){
         $_SESSION['quantity' . $i]  = $_SESSION['quantity' . $next];
     }
+    
+     
+}
     $_SESSION['basketcounter']--;
     echo '<script>location.href="', $_SERVER['HTTP_REFERER'], '"</script>';
 }
