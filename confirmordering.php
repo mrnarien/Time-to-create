52ed0295aff0d0be463d358a6c896901fab3ecc8
mrnarien <fable.3@yandex.ru>
2017-06-03 20:42:46 +0700

    Update confirmordering.php

--- confirmordering.php
+++ confirmordering.php
@@ -3,6 +3,7 @@ session_start();
 if (isset($_SESSION['login']) && isset($_SESSION['adminmode'])) {
     echo header('Location:index.php');
 } else {
+    $sum = 0;
     for ($i = 0; $i < $_SESSION['basketcounter']; $i++) {
         $sum = $sum + $_SESSION['price' . $i] * $_SESSION['quantity' . $i];
         file_put_contents('files/ordersitems.txt', '<tr><td>' . $_SESSION['item' . $i] . '</td><td>' . $_SESSION['item_name' . $i] . '</td><td>' . $_SESSION['quantity' . $i] . '</td><td>' . $_SESSION['price' . $i] * $_SESSION['quantity' . $i] . ' руб.</td></tr><br>', FILE_APPEND);
@@ -14,8 +15,6 @@ if (isset($_SESSION['login']) && isset($_SESSION['adminmode'])) {
     $email       = !empty($_GET['email']) ? htmlspecialchars($_GET['email']) : '';
     $address     = !empty($_GET['address']) ? htmlspecialchars($_GET['address']) : '';
     $date        = !empty($_GET['date']) ? htmlspecialchars($_GET['date']) : '';
-    date_default_timezone_set('Etc/GMT-6');
-    $orderingdate = date("d.m.y H:i");
     file_put_contents('files/orderslogins.txt', $login . "\n", FILE_APPEND);
     file_put_contents('files/ordersfullnames.txt', $fullname . "\n", FILE_APPEND);
     file_put_contents('files/ordersphonenumbers.txt', $phonenumber . "\n", FILE_APPEND);
