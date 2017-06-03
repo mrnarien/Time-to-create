28f50d9824601caca16052eb5954a77bd045c995
mrnarien <fable.3@yandex.ru>
2017-06-04 05:29:53 +0700

    Update login.php

--- login.php
+++ login.php
@@ -14,7 +14,6 @@
 						$passwords = file('files/passwords.txt', FILE_IGNORE_NEW_LINES);
 						for ($i = 0; $i < count($logins); $i++) {
 							if ($login == $logins[$i] && $password == $passwords[$i]) {
-								session_start();
 								$_SESSION['login'] = $login;
 								if ($_SESSION['login'] == 'daniil' && $password == '12344321') {
 									$_SESSION['adminmode'] = 'activated';
