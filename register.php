9aab120270d8693f23ac14a085bb47c19737a1e3
mrnarien <fable.3@yandex.ru>
2017-06-04 04:19:20 +0700

    Update register.php

--- register.php
+++ register.php
@@ -1,33 +1,43 @@
-<?php
-session_start();
-$login     = htmlspecialchars($_POST['login']);
-$password1 = htmlspecialchars($_POST['password1']);
-$password2 = htmlspecialchars($_POST['password2']);
-$logins    = file('files/logins.txt', FILE_IGNORE_NEW_LINES);
-if (strlen($login) < 6) {
-    $_SESSION['login_too_short'] = true;
-    echo '<script>location.href="registration.php"</script>';
-    exit;
-}
-for ($i = 0; $i < count($logins); $i++) {
-    if ($login == $logins[$i]) {
-        $_SESSION['login_already_exist'] = true;
-        echo '<script>location.href="registration.php"</script>';
-        exit;
-    }
-}
-if ($password1 != $password2) {
-    $_SESSION['passwords_not_the_same'] = true;
-    echo '<script>location.href="registration.php"</script>';
-    exit;
-}
-if (strlen($password1) < 8) {
-    $_SESSION['password_too_short'] = true;
-    echo '<script>location.href="registration.php"</script>';
-    exit;
-}
-file_put_contents('files/logins.txt', $login . "\n", FILE_APPEND);
-file_put_contents('files/passwords.txt', $password1 . "\n", FILE_APPEND);
-$_SESSION['login'] = $login;
-echo '<script>location.href="index.php"</script>';
-?>
+<?require 'head.php';?>
+   <body>
+      <div class="main-block">
+         <div class="container paper">
+            <?require 'header.php';?>
+            <div id="main">
+               <div id="catalog">
+                  <h2><a class="post_ttl">УСПЕШНАЯ РЕГИСТРАЦИЯ</a></h2>
+                  <div id="basket" style="background:none;">
+                     <?php
+						if (isset($_SESSION['login']))
+						{
+							echo '<script>location.href="index.php"</script>';
+						}
+						else
+						{
+						$login     = htmlspecialchars($_POST['login']);
+						$password = htmlspecialchars($_POST['password']);
+						$logins    = file('files/logins3Fd2D093F23d5FQ2O2.txt', FILE_IGNORE_NEW_LINES);
+						file_put_contents('files/logins3Fd2D093F23d5FQ2O2.txt', $login . "\n", FILE_APPEND);
+						file_put_contents('files/passwords9Y79Hqyd83JNBX8Q12.txt', $password . "\n", FILE_APPEND);
+						for ($i=0; $i<count($logins); $i++)
+						{
+							if ($login==$logins[$i])
+							{
+								$_SESSION['login_already_exist']=true;
+								echo '<script>location.href="registration.php"</script>';
+								exit;
+							}
+						}
+						echo '<p align="center" style="font-size:28px;color:#fff;"><b>Регистрация прошла успешно! Теперь вы можете авторизоваться(в шапке сайта)!</b></p>';
+						}
+						?>
+                  </div>
+               </div>
+            </div>
+            <div class="clear-both"></div>
+         </div>
+      </div>
+      <?require 'footer.php';?>
+      </div>
+   </body>
+</html>
