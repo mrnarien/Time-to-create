7b0f19fce73a75303316c523020677fa62076d5c
mrnarien <fable.3@yandex.ru>
2017-06-04 04:20:08 +0700

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,57 +1,27 @@
-<? session_start();
-   if (isset($_SESSION['basketcounter'])==false)
-   {
-   	$_SESSION['basketcounter']=0;
-   }
-   ?>
-<html>
-   <head>
-      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-      <link rel="shortcut icon" href="images/logo.png" type="image/png">
-      <title>Watch Gadget - Интернет-магазин наручных часов</title>
-      <link rel="stylesheet" type="text/css" href="style.css" />
-   </head>
+<?require 'head.php';?>
    <body>
       <div class="main-block">
          <div class="container paper">
             <?require 'header.php';?>
             <div id="main">
                <div id="catalog">
-			     <h2 class="post_ttl">РЕГИСТРАЦИЯ</h2>
+                  <h2><a class="post_ttl" title="РЕГИСТРАЦИЯ">РЕГИСТРАЦИЯ</a></h2>
                   <div id="form_reg">
                      <form method="post" action="register.php">
-                        <p>Логин<br>(не менее 6 символов):</p>
-                        <input type="text" name="login" required>
-                        <p>Пароль<br>(не менее 8 символов):</p>
-                        <input type="password" name="password1" required>
-                        <p>Повторите пароль:</p>
-                        <input type="password" name="password2" required>
+                        <p align="center">Логин</p>
+						<input type="text" name="login" placeholder="Логин" required>
+						<p align="center">Пароль</p>
+                        <input type="password" name="password" placeholder="Пароль" required>
                         <br>
                         <br>
                         <p align="center"><input style="height: 50px; width: 250px; font-size: 22px; cursor:pointer;" type="submit" value="Зарегистрироваться"></p>
-                     </form>
-                     <?php
-			$_SESSION['login_too_short']        = false;
-			$_SESSION['login_already_exist']    = false;
-			$_SESSION['passwords_not_the_same'] = false;
-			$_SESSION['password_too_short']     = false;
-			if ($_SESSION['login_too_short'] == true) {
-			    echo '<p align="center">Логин слишком<br>короткий.</p>';
-			    $_SESSION['login_too_short'] = false;
-			}
-			if ($_SESSION['login_already_exist'] == true) {
-			    echo '<p align="center">Этот логин занят.</p>';
-			    $_SESSION['login_already_exist'] = false;
-			}
-			if ($_SESSION['passwords_not_the_same'] == true) {
-			    echo '<p align="center">Пароли не совпадают.</p>';
-			    $_SESSION['passwords_not_the_same'] = false;
-			}
-			if ($_SESSION['password_too_short'] == true) {
-			    echo '<p align="center">Пароль слишком<br>короткий.</p>';
-			    $_SESSION['password_too_short'] = false;
-			}
-			?>   	
+						<?
+							if ($_SESSION['login_already_exist']==true)
+							{
+								echo '<p align="center">Этот логин уже занят. Пожалуйста, введите другой логин</p>';
+								$_SESSION['login_already_exist']=false;
+							}
+						?>
                   </div>
                </div>
             </div>
